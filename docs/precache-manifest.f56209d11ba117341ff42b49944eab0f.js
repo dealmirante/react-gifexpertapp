64d097b882bdcf5be541eba3eee7df12d3036270
dealmirante <dealmirante@gmail.com>
2020-09-08 18:39:56 -0300

    docs agregado

---
+++ docs/precache-manifest.f56209d11ba117341ff42b49944eab0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f423906071bc23f9d6b641ab28444c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2e7f85f5503e08ec6d0",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e7f85f5503e08ec6d0",
+    "url": "/static/js/main.1c61da87.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
