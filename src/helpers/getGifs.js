b0f37a5c50b4e1659a28c9e923b4cb2a6b8c9f8b
dealmirante <dealmirante@gmail.com>
2020-09-08 19:10:44 -0300

    Correccion de api key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,8 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    //const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=sUvpqaH6RHlxDJ6DMNxPWMsTr0DAGTxA`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
